29e87659d21cba62598e7c2c70567790523ff82f
Jeff Reiner <theundeadghost@gmail.com>
2020-04-06 18:14:54 +0200

    :sparkles: Building new logic

--- components/layout.js
+++ components/layout.js
@@ -2,7 +2,7 @@ import Div100vh from "react-div-100vh";
 import Head from "next/head";
 
 const Layout = ({ children }) => (
-  <Div100vh>
+  <Div100vh as="main" style={{display: "grid", placeItems: "center", height: '100rvh' }}>
     <Head>
       <title>Matte</title>
       <meta
--- pages/index.js
+++ pages/index.js
@@ -1,61 +1,92 @@
-import Layout from "../components/layout";
+import { Fragment, useRef, useState } from "react";
 import { Button } from "../components/buttons";
 import { FileInput } from "../components/inputs";
-import { useTransition, animated, config } from "react-spring";
-
-const Image = ({ src }) => {
-  const [state, toggle] = React.useState(true);
-
-  const transitions = useTransition(state, null, {
-    from: { opacity: 0, transform: "scale(0.8)" },
-    enter: { opacity: 1, transform: "scale(1)" },
-    leave: { opacity: 0, transform: "scale(0.8)" },
-    config: { mass: 1, tension: 180, friction: 15 }
-  });
-
-  return transitions.map(
-    ({ item, key, props }) =>
-      item && <animated.img className="img" src={src} key={key} style={props} />
-  );
-};
+import Layout from "../components/layout";
+import { CLOUDINARY_BASE } from "../lib/constants";
 
 const Page = () => {
-  const [file, setFile] = React.useState();
+  const [file, setFile] = useState(undefined);
+  const [imagePreview, setImagePreview] = useState(undefined);
+  const [loading, setLoading] = useState(false);
+  const downloadRef = useRef(null);
 
-  const handleFileChange = e => setFile(URL.createObjectURL(e.target.files[0]));
+  const handleFileChange = (e) => setFile(e.target.files[0]);
 
-  const clearFile = () => setFile();
+  const saveImage = () => downloadRef.current.click();
+
+  const clearFile = () => {
+    setImagePreview(undefined);
+    setFile(undefined);
+  };
+
+  const matFile = async () => {
+    setLoading(true);
+
+    try {
+      const post = await fetch("/api/upload", {
+        method: "POST",
+        headers: { "Content-Type": file.type },
+        body: file,
+      });
+
+      const { public_id } = await post.json();
+
+      setImagePreview(CLOUDINARY_BASE + public_id);
+      setLoading(false);
+    } catch (err) {
+      setLoading(false);
+      console.error(err);
+      window.alert(err.message);
+    }
+  };
 
   return (
     <Layout>
-      {!!file && (
-        <div className="top-right">
-          <Button onClick={clearFile}>New</Button>
-        </div>
+      {Boolean(file) && (
+        <Fragment>
+          <div className="top-right">
+            <Button onClick={clearFile}>New</Button>
+          </div>
+
+          <div className="bottom-centered">
+            {!Boolean(imagePreview) ? (
+              <Button onClick={matFile}>
+                {loading ? "Matting..." : "Matte"}
+              </Button>
+            ) : (
+              <Button onClick={saveImage}>Save</Button>
+            )}
+          </div>
+        </Fragment>
       )}
 
-      {/* {!!file && (
-        <div className="bottom-centered">
-          <Button>Save</Button>
-        </div>
-      )} */}
+      <div>
+        {/* Preview Mated Image */}
+        {Boolean(imagePreview) && (
+          <Fragment>
+            <img className="img" src={imagePreview} alt="" />
+            <a href={imagePreview} download hidden ref={downloadRef}>
+              Download
+            </a>
+          </Fragment>
+        )}
 
-      <div className="content">
-        {!file && <FileInput onChange={handleFileChange} />}
+        {/* Preview Uploaded Image */}
+        {Boolean(file) && !Boolean(imagePreview) && (
+          <img className="img" src={URL.createObjectURL(file)} alt="" />
+        )}
 
-        {!!file && <Image src={file} alt="" />}
+        {/* Upload File */}
+        {!Boolean(file) && <FileInput onChange={handleFileChange} />}
       </div>
 
       <style jsx global>{`
         .img {
-          background-color: white;
           width: 320px;
           height: 320px;
           object-fit: contain;
         }
-      `}</style>
 
-      <style jsx>{`
         .top-right {
           position: fixed;
           top: 24px;
@@ -67,12 +98,6 @@ const Page = () => {
           bottom: 32px;
           transform: translateX(-50%);
           left: 50%;
-        }
-
-        .content {
-          display: grid;
-          place-items: center;
-          height: 100%;
         }
       `}</style>
     </Layout>
