c6a45f3d7e10ba5c459dc2177d510c2af20a221e
Jeff Reiner <gone@hey.com>
2020-07-18 10:58:15 +0200

    Add Support link

---
+++ svgs/Buoy.js
@@ -0,0 +1,16 @@
+export default ({ size = 24 }) => (
+  <svg
+    width={size}
+    height={size}
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth={2}
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <circle cx={12} cy={12} r={10} />
+    <circle cx={12} cy={12} r={4} />
+    <path d="M4.93 4.93l4.24 4.24M14.83 14.83l4.24 4.24M14.83 9.17l4.24-4.24M14.83 9.17l3.53-3.53M4.93 19.07l4.24-4.24" />
+  </svg>
+);
